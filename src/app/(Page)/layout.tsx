40906a5f73e7545caa6ac43b6f5188cf7a5b9980
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-05-29 00:17:14 +0200

    feat(layout, page): add layout with desired fonts

--- src/app/(Page)/layout.tsx
+++ src/app/(Page)/layout.tsx
@@ -8,6 +8,19 @@ import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import { fetchFooter, fetchHeader } from '@/sanity/lib/queries';
 import { client } from '@/sanity/lib/client';
+import { Oswald, Lato } from 'next/font/google';
+
+const oswald = Oswald({
+  variable: '--font-oswald',
+  subsets: ['latin'],
+  weight: ['600'],
+});
+
+const lato = Lato({
+  variable: '--font-lato',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default async function RootLayout({
   children,
@@ -18,7 +31,9 @@ export default async function RootLayout({
   const footer = await client.fetch(fetchFooter);
   return (
     <html lang='en'>
-      <body className={`flex flex-col `}>
+      <body
+        className={`flex flex-col ${oswald.variable} ${lato.variable} antialiased`}
+      >
         <Header data={header} />
         <main className='flex-1'>{children}</main>
         <Footer data={footer} />
