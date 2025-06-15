a330196ab3931a1ab4c19dea9513a4bc157e2802
Marvin private <marvin96k@gmail.com>
2025-06-15 18:47:25 +0200

    feat(MovieDetailHero): implement MovieDetailHero component for enhanced movie display
    - Replaced the existing movie detail layout with a new `MovieDetailHero` component for improved presentation.
    - Integrated movie banner, title, description, and action buttons into the new component.
    - Streamlined the main content structure for better responsiveness and user experience.

---
+++ src/components/Blocks/DistributionList/DistributionMovieDetail/MovieDetailHero.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FetchDistributionMovieResult } from '../../../../../sanity.types';
+import SanityImage from '@/components/Media/SanityImage';
+import RichText from '@/components/RichText/RichText';
+import Button from '@/components/Button/Button';
+
+const MovieDetailHero = (movie: FetchDistributionMovieResult) => {
+  if (!movie) return null;
+  const { title, movieBanner, description, button, ticket } = movie;
+
+  return (
+    <section className='relative grid max-lg:top-0 max-lg:left-0 max-lg:right-0 max-lg:items-center z-0'>
+      <div className='flex flex-col justify-end relative max-lg:h-screen lg:grid lg:grid-cols-24 lg:gap-2'>
+        <div className='relative flex flex-col gap-8 z-10 max-lg:px-p-mobile max-lg:pb-p-mobile lg:px-p-desktop lg:col-span-full lg:flex-row lg:justify-between '>
+          <div className='grid gap-4 lg:whitespace-nowrap'>
+            <h1 className='text-h-50 xl:text-h-67 leading-none lg:leading-normal uppercase'>
+              {title}
+            </h1>
+            <div className='block lg:hidden'>
+              {description && (
+                <RichText content={description} className='!line-clamp-[7]' />
+              )}
+            </div>
+          </div>
+          <div className='flex gap-6 lg:items-center'>
+            {ticket && (
+              <div className='flex'>
+                <Button
+                  href={ticket?.ticketLink?.href ?? ''}
+                  label={ticket?.ticketLinkLabel ?? 'Biljetter'}
+                  className='ticket-button'
+                />
+              </div>
+            )}
+            {button && (
+              <div className='flex'>
+                <Button
+                  href={button?.buttonLink?.href ?? ''}
+                  label={
+                    button?.buttonLabel ? button.buttonLabel : 'Pressmaterial'
+                  }
+                />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className='absolute inset-0 h-full lg:relative lg:h-auto lg:col-span-full'>
+          {movieBanner?.media && (
+            <SanityImage
+              {...movieBanner}
+              className='h-full lg:aspect-video z-0'
+            />
+          )}
+          {/* Bottom fade  */}
+          <div className='block absolute bottom-0 w-full h-2/4 bg-gradient-to-t from-background via-background/60 to-transparent pointer-events-none z-0' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MovieDetailHero;
