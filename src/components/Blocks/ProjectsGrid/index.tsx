5611b5e21d0f5e4e06ace1301045f0f3d26226e2
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-09-30 22:27:35 +0200

    refactor(ProjectsGrid): extract FeaturedProjectCard into separate component
    - Replaced inline rendering of the featured project with a dedicated `FeaturedProjectCard` component for improved modularity and readability.
    - Removed unused imports and streamlined the `ProjectsGrid` component.
    - Enhanced the layout and structure of the featured project display logic.

--- src/components/Blocks/ProjectsGrid/index.tsx
+++ src/components/Blocks/ProjectsGrid/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { client } from '@/sanity/lib/client';
 import { fetchAllProjects } from '@/sanity/lib/queries';
-import SanityImage from '@/components/Media/SanityImage';
-import RichText from '@/components/RichText/RichText';
 import ProjectCard from './ProjectsCard';
+import FeaturedProjectCard from './FeaturedProjectCard';
 import type { Projects } from '../../../../sanity.types';
-import { SanityImageObject } from '@/components/Media/SanityImage/SanityImageObject';
 
 type IProjectsGrid = {
   showFeaturedProjectCard?: boolean;
@@ -21,25 +19,7 @@ const ProjectsGrid = async ({ showFeaturedProjectCard }: IProjectsGrid) => {
   return (
     <section className='page-x-spacing grid gap-2'>
       {showFeaturedProjectCard && projectToDisplay && (
-        <div className='container-fluid flex flex-col gap-6 lg:flex-row p-2.5 bg-dark-gray rounded-lg lg:gap-0'>
-          {projectToDisplay?.projectImage && (
-            <SanityImage
-              {...(projectToDisplay.projectImage as unknown as SanityImageObject)}
-              className='rounded-lg lg:w-2/3'
-              aspectRatio='16/9'
-            />
-          )}
-          <div className='flex flex-col gap-5 max-lg:pb-2.5 lg:flex-1 lg:px-10 lg:py-6'>
-            <h2 className='font-oswald text-h-21 lg:text-h-28 xl:text-h-37'>
-              {projectToDisplay?.title}
-            </h2>
-            {projectToDisplay?.description && (
-              <div className='text-b-16'>
-                <RichText content={projectToDisplay?.description} />
-              </div>
-            )}
-          </div>
-        </div>
+        <FeaturedProjectCard project={projectToDisplay} />
       )}
       <div className='grid gap-x-2 gap-y-5 grid-cols-1 md:grid-cols-2 lg:gap-y-10 lg:grid-cols-3'>
         {projectsForGrid.map((project, index) => {
