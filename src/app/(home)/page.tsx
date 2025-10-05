60210aee9126303c1de3f56101c0e890dccf64f6
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-10-05 20:52:31 +0200

    fix(HomePage): adjust padding based on hero block presence
    - Updated the HomePage component to conditionally set padding based on the presence of a heroCarousel block.
    - Introduced a check for the first block type to enhance layout responsiveness.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,10 +24,16 @@ export default async function HomePage() {
       'http://localhost:3000',
   });
 
+  const firstBlock = data.blockList[0];
+  const isHero =
+    firstBlock && '_type' in firstBlock && firstBlock._type === 'heroCarousel';
+
   return (
     <>
       <JsonLd data={orgJsonLd} />
-      <div className={`grid grid-cols-1 gap-8 pt-[22%]`}>
+      <div
+        className={`grid grid-cols-1 gap-8 ] ${isHero ? 'pt-0' : 'max-lg:pt-[22%]'}`}
+      >
         {data.blockList.map((block, idx) => {
           if (!('_type' in block)) return null;
           return (
