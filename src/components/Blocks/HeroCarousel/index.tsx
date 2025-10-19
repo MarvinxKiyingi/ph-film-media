273333afa2398bba9dfe8c1ca211b52cf9823294
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-10-19 07:55:34 +0200

    fix(HeroCarousel): update height styles for better viewport compatibility

--- src/components/Blocks/HeroCarousel/index.tsx
+++ src/components/Blocks/HeroCarousel/index.tsx
@@ -14,7 +14,7 @@ const HeroCarousel = async ({
   return (
     <section
       key={idx}
-      className='max-lg:min-h-screen h-dvh lg:h-[calc(100dvh-2*var(--spacing-p-desktop))] lg:overflow-hidden'
+      className='max-lg:min-h-[100svh] h-[100svh] lg:h-[calc(100vh-2*var(--spacing-p-desktop))] lg:overflow-hidden'
     >
       <HeroCarouselBlock block={block} />
     </section>
